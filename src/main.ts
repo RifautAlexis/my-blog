a4e4a79a0dd7a4f32a01683ceff3eccfc6cf42e2
Rifaut Alexis <ralexis149@gmail.com>
2023-05-30 13:04:38 +0200

    Remove non-destructive hydration

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,15 @@
-import { bootstrapApplication, provideClientHydration } from '@angular/platform-browser';
+import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { importProvidersFrom } from '@angular/core';
-import { RouterModule, TitleStrategy } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { routes } from './app/app-routing.module';
-import { HttpClientModule, provideHttpClient } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { MatNativeDateModule } from '@angular/material/core';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideAnimations(),
     provideHttpClient(),
-    provideClientHydration(),
     importProvidersFrom(
       RouterModule.forRoot(routes)
     ),
