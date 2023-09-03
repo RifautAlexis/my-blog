2dc23d3b191d45074d36829b1f0a2c766050ad48
Rifaut Alexis <ralexis149@gmail.com>
2023-09-03 18:46:28 +0200

    WIP

--- src/app/features/admin/services/admin.service.ts
+++ src/app/features/admin/services/admin.service.ts
@@ -8,17 +8,41 @@ import {
 import { Article } from 'src/app/shared/models/article';
 import { ArticleService } from 'src/app/shared/services/article.service';
 
+export enum Status {
+  Success,
+  Loading,
+  Error,
+}
+
+export interface State<T> {
+  status: Status;
+  result: T;
+}
+
 @Injectable({ providedIn: 'root' })
 export class AdminService {
   private articleService = inject(ArticleService);
 
-  articles: WritableSignal<Article[]> = signal<Article[]>([]);
-  initialData: Article[] = [];
+  initialData: State<Article[]> = {status: Status.Loading, result: []};
+  articles: WritableSignal<State<Article[]>> = signal<State<Article[]>>(this.initialData);
 
   init() {
-    this.articleService.getArticles().subscribe((data) => {
-      this.initialData = data;
-      this.articles.set(data);
+    // this.articleService.getArticles().subscribe((data) => {
+    //   this.initialData = data;
+    //   this.articles.set(data);
+    // });
+    
+    this.articleService.getArticles().subscribe({
+      next(value) {
+        var data: State<Article[]> = {status: Status.Success, result: value} as State<Article[]>;
+        this.initialData = data;
+        this.articles.set(data);
+      },
+      error(err) {
+        var data: State<Article[]> = {status: Status.Error, result: []} as State<Article[]>;
+        this.initialData = data;
+        this.articles.set(data);
+      },
     });
   }
 
